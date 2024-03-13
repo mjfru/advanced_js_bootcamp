a345abf97626c3eb02045883efd579e9c089dcfb
Matt Frutchey <mjfru201@gmail.com>
2024-03-13 15:02:44 -0400

    complete inheritance / super / static notes

--- js_oop/03inheritance_super_static.js
+++ js_oop/03inheritance_super_static.js
@@ -19,4 +19,58 @@ We need a way to call the constructor from the parent...but add our own new prop
   ?   super(a, b);          <--- using the parent class
   ?   this.c = c;           <--- adding a new property
   ? };
+
+
+! Static Properties
+* Modern JS also offers "static properties", where individual pieces of data are on the class, not on instances.
+? Other OO languages often all this idea a "class attribute".
+
+Examples:
 */
+class CatWithStaticProp {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+  // Good example of a static property: All instances of cats are the same species, it doesn't vary one to another!
+  // It will never be tied to any instance of this class, it'll always be the same and be included with every instance instantiated in this class.
+  static genusSpecies = "feline catus";
+
+  describe() {
+    return `${this.name} is a ${CatWithStaticProp.genusSpecies}.`;
+  }
+
+  //? Static Method:
+  static meow() {
+    return "MEOOOOOOOW";
+  }
+}
+
+console.log(CatWithStaticProp.genusSpecies);
+cat = new CatWithStaticProp("Tom", "Tabby");
+console.log(cat.name);
+console.log(cat.describe());
+// cat.meow() will NOT work, it will only work as CatWithStaticProp.meow();
+
+/*
+! Static Methods
+* Static methods, similar to static properties, are methods that are called on a Class, not an object!
+*   Therefore, it cannot have access to individual object attributes.
+? In a static method, 'this' will refer to...the entire class itself.
+Almost every other OO language more properly calls this a 'class method' since it does have access to the class itself.
+
+? So, when would we ever use these?
+When we want to group together related functionality...think about the Math object that we can use and access within JavaScript!
+These are all functions that could exist on their own, but it's more convenient to have them grouped together.
+
+Another common use case if to create factory methods.
+These can help us in the create of a new instance of a class.
+*/
+class User {
+  static registerUser(username, password) {
+    return {
+      username: username,
+      password: password
+    }
+  }
+}
