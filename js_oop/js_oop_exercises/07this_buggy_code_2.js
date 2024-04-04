10796e43f13b0e2cfbb4ae45b3a1c0bbb5b92793
Matt Frutchey <mjfru201@gmail.com>
2024-04-04 08:01:18 -0400

    fix code in exercise #2

---
+++ js_oop/js_oop_exercises/07this_buggy_code_2.js
@@ -0,0 +1,29 @@
+/*
+! Buggy Code 'this' Timers Exercise
+In the Timer class, modify the start method to log the tick property at one - second intervals correctly and add a stop method to clear the interval.
+
+The timer should stop after 5 ticks.
+*/
+
+class Timer {
+  constructor() {
+    this.tick = 0;
+    this.timerId = null;
+  }
+
+  start() {
+      this.timerId = setInterval(() => {
+        console.log(this.tick++);
+        if (this.tick === 5) {
+          this.stop();
+        }
+      }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.timerId);
+  }
+}
+
+const timer = new Timer();
+timer.start();
